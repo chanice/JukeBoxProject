c1dc5f3483d802c250bfdb227787a3478fcafd1d
Danaya Melendez <danayamel@me.com>
2018-01-11 11:28:48 -0500

    First commit for dmel

--- jukebox.js
+++ jukebox.js
@@ -1,23 +1,23 @@
-function Jukebox(song){
-
-}
+// function Jukebox(song){
+//
+// }
 
 
 function Song (title, currentTime, songLink){
 	this.title = title;
 	this.currentTime = currentTime;
 	this.songLink = songLink;
-};
-counter = 0; 
-song1 = new Song("PrettyWings", 0, "PrettyWings.mp3")
+}
+counter = 0;
+song1 = new Song("PrettyWings", 0, "PrettyWings.mp3");
 
-song2 = new Song("Sweet Lady", 0, "SweetLady.mp3")
+song2 = new Song("Sweet Lady", 0, "SweetLady.mp3");
 
-song3 = new Song("MyGoodies", 0, "MyGoodies.mp3")
+song3 = new Song("MyGoodies", 0, "MyGoodies.mp3");
 
-var songList = [song1, song2, song3]
+var songList = [song1, song2, song3];
 
-var x = document.getElementById("audio")
+var x = document.getElementById("audio");
 function playAudio(){
 	x.play();
 
@@ -36,4 +36,3 @@ function skipAhead(){
 	counter++;
 	document.getElementById("audio").innerHTML = "<audio controls id = 'audio'> <source src=\"" + songList[counter].songLink + "></audio>"
 }
-
