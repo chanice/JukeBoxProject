9c58a4f0b2cc8dc16bd52257efb4d1f1faf5a72a
Danaya Melendez <danayamel@me.com>
2018-01-11 12:20:23 -0500

    added a playlist

--- jukebox.js
+++ jukebox.js
@@ -1,19 +1,16 @@
-// function Jukebox(song){
-//
-// }
 
-
-function Song (title, currentTime, songLink){
+function Song (title, currentTime, songLink, id){
 	this.title = title;
 	this.currentTime = currentTime;
 	this.songLink = songLink;
+	this.id = id;
 }
 counter = 0;
-song1 = new Song("PrettyWings", 0, "PrettyWings.mp3");
+song1 = new Song("PrettyWings", 0, "PrettyWings.mp3", 'pw');
 
-song2 = new Song("Sweet Lady", 0, "SweetLady.mp3");
+song2 = new Song("Sweet Lady", 0, "SweetLady.mp3", 'sl');
 
-song3 = new Song("MyGoodies", 0, "MyGoodies.mp3");
+song3 = new Song("MyGoodies", 0, "MyGoodies.mp3", 'mg');
 
 var songList = [song1, song2, song3];
 
@@ -31,15 +28,20 @@ function pauseAudio(){
 function displaySong(song){
 	document.getElementById("currentSong").innerText = song.title;
 }
-displaySong(song1)
-console.log()
 
 function skipAhead(){
 	counter++;
 	document.getElementById("audioDiv").innerHTML = "<audio id = \"audio\" src=\"" + songList[counter].songLink + "\"></audio>"
 	displaySong(songList[counter]);
+	getCurrSong(songList[counter]);
 	playAudio();
+	getCurrSong();
+}
+
+function getCurrSong(song){
+	document.getElementById("current").innerText = song.title;
+	document.getElementById(song.id).style.fontSize = "20px";
+
 }
 
 // console.log(songList[counter].songLink)
-
